b844810aca49aec257d7d328f4af657e7e4bc92a
ASproson <a.sproson@outlook.com>
2022-05-02 14:29:50 +0100

    Adds C>F temp conversion demo

--- demo.swift
+++ demo.swift
@@ -1,9 +1,22 @@
 print("hello world")
 
-// Type enforcement
+// Type enforcement && variable assignment
 var book: String = "Just Kids"
 var pageCount: Int = 278
+var tempf: Double = 83.0 // Floats
+var truth: Bool = true
+truth = false
 
 // String interpolation
 var age = 20
 print("I am \(age) years old.")
+
+// Constant assignment
+let birthMonth = "January"
+
+// C>F Temp calculation demo
+var tempf: Double = 83.0
+var tempc: Double = 0.0
+tempc = (tempf - 32) / 1.8
+
+print("The temp is \(tempc) degrees celsius")
